a87800a26ee7546ed4cec082d5e83f0974be26fa
Juan18 <juan_camilo1018@hotmail.com>
2023-05-29 00:20:13 -0500

    vers 3

--- Links utiles.R
+++ Links utiles.R
@@ -1,18 +1,40 @@
 
-El modelo de regresión lineal simple
+# El modelo de regresión lineal simple:
 
 
-`highlight` argument must be one of `default`, `tango`, `pygments`, `kate`, 
-`monochrome`, `espresso`, `zenburn`, `haddock`, `breezedark`, `textmate`, 
-`arrow`, or `rstudio` or a file with extension `.theme`.
+# `highlight` argument must be one of `default`, `tango`, `pygments`, `kate`, 
+# `monochrome`, `espresso`, `zenburn`, `haddock`, `breezedark`, `textmate`, 
+# `arrow`, or `rstudio` or a file with extension `.theme`.
 
 
-arrow
-breezedark
-zenburn
+# arrow
+# breezedark
+# zenburn
+
+
+# https://github.com/cran/rmdformats
+# https://github.com/juba/rmdformats
+
+# color: https://stackoverflow.com/questions/63110500/modifying-readthedown-markdown
+
+
+# Plantillas Rmarkdown: https://rmd.pzhao.org/portfolio/
+
+# https://www.datadreaming.org/post/r-markdown-theme-gallery/
+
+
+# https://github.com/yixuan/prettydoc
+
+
+# Dasboards: https://themeon.net/nifty/v3.0.1/#
 
 
 
+
+########### libro:
+# https://juba.github.io/tidyverse/18-purrr.html
+
+#######
 
 
 
